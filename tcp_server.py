60979ffa7d7035d31eed8fd9b621cc0ff8bfa367
obote256 <167576906+obote256@users.noreply.github.com>
2025-05-26 18:12:13 -0700

    added client-handling thread

--- tcp_server.py
+++ tcp_server.py
@@ -3,4 +3,14 @@ import socket
 bind_ip = "0.0.0.0"
 bind_port = 4444
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind port))
+server.listen(5)
+print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+#our client-handling thread
+def handle_client(client_socket):
+  #print out what the client sends
+  request = client_socket.recv(1024)
+  print :[*] Recieved: %s: % request
